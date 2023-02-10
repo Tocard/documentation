a2645aabc1b5d20a84a919d3832c4f815c12c295
nicoburkart <nicoburkart@web.de>
2023-02-10 11:33:56 -0500

    fixed edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/streamr-dev/documentation/tree/main/docs',
+          editUrl: 'https://github.com/streamr-dev/documentation/blob/main',
         },
         blog: {
           showReadingTime: true,
